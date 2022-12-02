689bc203d1b5c1251c4f76bfbfb986e80c610f34
Paul Thebaud <paul.thebaud29@gmail.com>
2022-12-02 12:28:05 +0100

    fix: forId typing

--- src/core/actions/context/enhancers/forId.ts
+++ src/core/actions/context/enhancers/forId.ts
@@ -3,6 +3,6 @@ import context from '@/core/actions/context/enhancers/context';
 import { ActionContext } from '@/core/actions/types';
 import { ModelId } from '@/core/model/types';
 
-export default function forId(id: ModelId | undefined) {
+export default function forId<ID extends ModelId | undefined>(id: ID) {
   return <C extends ActionContext>(a: Action<C>) => a.use(context({ id }));
 }
